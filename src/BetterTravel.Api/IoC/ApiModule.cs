254daa2686acce38a20b6ab7f80f7ce9634335a7
Alex K <a.katrynets@gmail.com>
2020-06-20 16:10:22 +0300

    added ioc

---
+++ src/BetterTravel.Api/IoC/ApiModule.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Autofac;
+using BetterTravel.Api.ExceptionHandling;
+using BetterTravel.Api.ExceptionHandling.Abstractions;
+using MediatR;
+
+namespace BetterTravel.Api.IoC
+{
+    [ExcludeFromCodeCoverage]
+    public class ApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            RegisterExceptionHandling(builder);
+            RegisterMediator(builder);
+        }
+
+        private void RegisterExceptionHandling(ContainerBuilder builder)
+        {
+            builder.RegisterType<ExceptionRequestHandler>()
+                .As<IExceptionRequestHandler>();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .AsClosedTypesOf(typeof(IExceptionHandler<>))
+                .As(typeof(IExceptionHandler<>));
+        }
+
+        private void RegisterMediator(ContainerBuilder builder)
+        {
+            builder.RegisterType<Mediator>()
+                .As<IMediator>()
+                .InstancePerLifetimeScope();
+
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+        }
+    }
+}
