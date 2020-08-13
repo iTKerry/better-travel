a0c1c148fdaeb334992b807e8f0c569007430552
Alex K <alexk@varyence.com>
2020-08-13 14:06:04 +0300

    wip

--- src/BetterTravel.Application/HotTours/Providers/Poehalisnami/Responses/PoehalisnamiResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BetterTravel.Application.HotTours.Providers.Poehalisnami.Responses
-{
-    public class PoehalisnamiResponse
-    {
-        public List<TourListItemResponse> TourListItems { get; set; }
-    }
-}
