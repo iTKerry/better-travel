de2e31d5c7bfd8f660bb0fdb95455d9a61512499
Alex <a.katrynets@gmail.com>
2021-04-09 14:23:37 +0300

    add new provider

---
+++ src/Services/Tours/ToursProvider/Application.fs
@@ -0,0 +1,43 @@
+﻿[<AutoOpen>]
+module Application
+
+open System
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+type AppState =
+    { CliArguments    : string array option
+      HostConfigs     : (IHostBuilder -> IHostBuilder) list
+      ServicesConfigs : (IServiceCollection -> IServiceCollection) list }
+    
+type AppBuilder internal () =
+    member _.Yield _ =
+        { CliArguments = None
+          HostConfigs = []
+          ServicesConfigs = [] }
+        
+    member _.Run (state : AppState) : IHostBuilder =
+        let host =
+            Host.CreateDefaultBuilder(Option.toObj state.CliArguments)
+            |> List.foldBack id state.HostConfigs
+        
+        host.ConfigureServices (fun svcs ->
+            state.ServicesConfigs
+            |> List.rev
+            |> List.iter (fun fn -> fn svcs |> ignore))
+        
+    [<CustomOperation("host_config")>]
+    member _.HostConfig (state, config) =
+        { state with HostConfigs = config::state.HostConfigs }
+        
+    [<CustomOperation("service_config")>]
+    member _.ServiceConfig(state, config) =
+        { state with ServicesConfigs = config::state.ServicesConfigs }
+    
+    [<CustomOperation("cli_args")>]
+    member _.CliArguments (state, args) =
+        { state with CliArguments = Some args }
+    
+let application = AppBuilder()
+
+let run (app: IHostBuilder) = app.Build().Run()
---
+++ src/Services/Tours/ToursProvider/Program.fs
@@ -0,0 +1,45 @@
+﻿module Program
+
+open System
+open System.Threading.Tasks
+open System.Timers
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Application
+
+type TestService(timer : Timer, logger : ILogger<TestService>) =
+    member private this.doWork _ =
+        logger.LogInformation "Doing work."
+    
+    interface IHostedService with
+        member this.StartAsync _ =
+            logger.LogInformation "Timed Hosted Service running."
+            timer.Elapsed.Add this.doWork
+            timer.Start ()
+            Task.CompletedTask
+        
+        member this.StopAsync _ =
+            logger.LogInformation "Timed Hosted Service is stopping."
+            timer.Stop ()
+            Task.CompletedTask 
+            
+    interface IDisposable with
+        member this.Dispose() =
+            timer.Dispose()
+
+let configureLogging (builder : ILoggingBuilder) = 
+    builder.AddConsole().AddDebug() |> ignore
+    
+let app argv =
+    application {
+        cli_args argv
+        host_config (fun cfg -> cfg.ConfigureLogging configureLogging)
+        service_config (fun cfg -> cfg.AddScoped<Timer>(fun _ -> new Timer(TimeSpan.FromSeconds(2.0).TotalMilliseconds)))
+        service_config (fun cfg -> cfg.AddHostedService<TestService>())
+    }
+    
+[<EntryPoint>]
+let main argv =
+    app argv |> run
+    0
