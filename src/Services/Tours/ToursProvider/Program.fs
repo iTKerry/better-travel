40b425fb855e53c04db9920cef5fa82dc6e73db9
Alex <a.katrynets@gmail.com>
2021-04-10 00:10:45 +0300

    add rest sharp and authentication tests

--- src/Services/Tours/ToursProvider/Program.fs
+++ src/Services/Tours/ToursProvider/Program.fs
@@ -1,12 +1,18 @@
 ﻿module Program
 
 open System
+open System.Collections.Generic
+open System.Net
+open System.Linq
+open System.Net.Http
 open System.Threading.Tasks
 open System.Timers
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Application
+open RestSharp
+open RestSharp
 
 [<AbstractClass>]
 type TimedHostedService(timer : Timer, logger : ILogger) =
@@ -31,6 +37,28 @@ type TimedHostedService(timer : Timer, logger : ILogger) =
 type TestService(logger : ILogger<TestService>) =
     inherit TimedHostedService(new Timer(TimeSpan.FromSeconds(5.0).TotalMilliseconds), logger)
     override this.doWork _ =
+        let providerUrl = "https://crm.*.ua/"
+        let x = Uri($"{providerUrl}/LiveTourSearch/GetDirections")
+        let x = Uri($"{providerUrl}/user/login")
+
+        let content =
+            [ Parameter("Login",      "", ParameterType.GetOrPost)
+              Parameter("Password",   "",         ParameterType.GetOrPost)
+              Parameter("RememberMe", "false",       ParameterType.GetOrPost) ]
+            
+        let client = RestClient(providerUrl, Timeout = -1)
+        let request = RestRequest("/user/login", Method.POST, AlwaysMultipartFormData = true)
+        request.Parameters.AddRange(content)
+        let response = client.Execute(request)
+        let cookies = response.Cookies |> Seq.toList
+        
+        let request = RestRequest("/api/Crm/ViewModeIsExistItemsGetInfo", Method.POST)
+        cookies |> List.iter (fun cookie -> request.AddCookie(cookie.Name, cookie.Value) |> ignore)
+        let request = request.AddParameter("currentUserId", "5072")
+        
+        let response = client.Execute(request)
+        logger.LogInformation response.Content
+                
         logger.LogInformation "Doing work."
 
 let configureLogging (builder : ILoggingBuilder) = 
