022f727a889d69aa94bd1dbf52fad2f97843964c
Alex <a.katrynets@gmail.com>
2021-04-10 12:48:39 +0300

    update

---
+++ src/Services/Tours/ToursProvider/HostedServices.fs
@@ -0,0 +1,27 @@
+﻿module HostedServices
+
+open System
+open System.Threading.Tasks
+open System.Timers
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+[<AbstractClass>]
+type TimedHostedService(timer : Timer, logger : ILogger) =
+    abstract member doWork : obj -> unit
+    
+    interface IHostedService with
+        member this.StartAsync _ =
+            logger.LogInformation "Timed Hosted Service running."
+            timer.Elapsed.Add this.doWork
+            timer.Start ()
+            Task.CompletedTask
+        
+        member this.StopAsync _ =
+            logger.LogInformation "Timed Hosted Service is stopping."
+            timer.Stop ()
+            Task.CompletedTask 
+            
+    interface IDisposable with
+        member this.Dispose() =
+            timer.Dispose()
--- src/Services/Tours/ToursProvider/Program.fs
+++ src/Services/Tours/ToursProvider/Program.fs
@@ -1,65 +1,82 @@
 ﻿module Program
 
+module Configs =
+    let loginCredentials =
+        [ "Login",      "тестировщик";
+          "Password",   "111";
+          "RememberMe", "false" ]
+        |> Map.ofList
+
+module Urls = 
+    let baseUrl = "https://crm.*.ua"
+    let loginUri = $"{baseUrl}/user/login"
+    let getListUri = $"{baseUrl}/TourOffer/GetList"
+
+
 open System
-open System.Collections.Generic
 open System.Net
-open System.Linq
-open System.Net.Http
-open System.Threading.Tasks
 open System.Timers
-open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
-open Application
-open RestSharp
+open Microsoft.Extensions.DependencyInjection
 open RestSharp
 
-[<AbstractClass>]
-type TimedHostedService(timer : Timer, logger : ILogger) =
-    abstract member doWork : obj -> unit
+open Application
+open HostedServices
+
+
+let client (timeout : int) (providerUrl : string) =
+    RestClient(providerUrl, Timeout = TimeSpan.FromSeconds(float timeout).Milliseconds)
     
-    interface IHostedService with
-        member this.StartAsync _ =
-            logger.LogInformation "Timed Hosted Service running."
-            timer.Elapsed.Add this.doWork
-            timer.Start ()
-            Task.CompletedTask
+let defaultClient providerUrl =
+    client 10 providerUrl
+
+let executeRequestAsync (client : RestClient, request : RestRequest) =
+    async {
+        match! client.ExecuteAsync(request) |> Async.AwaitTask with
+        | response when response.StatusCode = HttpStatusCode.OK ->
+            return response |> Ok 
+        | response when response.StatusCode <> HttpStatusCode.OK ->
+            return Error $"CODE: {response.StatusDescription}; MSG: {response.ErrorMessage}"
+        | response ->
+            return Error $"CODE: {response.StatusDescription}; MSG: {response.ErrorMessage}"
+    }
+
+let loginCookiesStealer (url : string) (content : Map<string, _>) =
+    async {
+        let request = RestRequest(url, Method.POST, AlwaysMultipartFormData = true)
+        content |> Map.iter (fun k v -> request.AddParameter(k, v) |> ignore)
         
-        member this.StopAsync _ =
-            logger.LogInformation "Timed Hosted Service is stopping."
-            timer.Stop ()
-            Task.CompletedTask 
-            
-    interface IDisposable with
-        member this.Dispose() =
-            timer.Dispose()
+        match! executeRequestAsync (defaultClient url, request) with
+        | Ok response -> return response.Cookies |> Seq.toList |> Ok
+        | Error error -> return Error error }
 
 type TestService(logger : ILogger<TestService>) =
-    inherit TimedHostedService(new Timer(TimeSpan.FromSeconds(5.0).TotalMilliseconds), logger)
+    inherit TimedHostedService(new Timer(TimeSpan.FromSeconds(10.0).TotalMilliseconds), logger)
     override this.doWork _ =
-        let providerUrl = "https://crm.*.ua/"
-        let x = Uri($"{providerUrl}/LiveTourSearch/GetDirections")
-        let x = Uri($"{providerUrl}/user/login")
-
-        let content =
-            [ Parameter("Login",      "", ParameterType.GetOrPost)
-              Parameter("Password",   "",         ParameterType.GetOrPost)
-              Parameter("RememberMe", "false",       ParameterType.GetOrPost) ]
+        async {
+            logger.LogInformation "Doing work."
             
-        let client = RestClient(providerUrl, Timeout = -1)
-        let request = RestRequest("/user/login", Method.POST, AlwaysMultipartFormData = true)
-        request.Parameters.AddRange(content)
-        let response = client.Execute(request)
-        let cookies = response.Cookies |> Seq.toList
-        
-        let request = RestRequest("/api/Crm/ViewModeIsExistItemsGetInfo", Method.POST)
-        cookies |> List.iter (fun cookie -> request.AddCookie(cookie.Name, cookie.Value) |> ignore)
-        let request = request.AddParameter("currentUserId", "5072")
-        
-        let response = client.Execute(request)
-        logger.LogInformation response.Content
+            match! loginCookiesStealer Urls.loginUri Configs.loginCredentials with
+            | Ok cookies ->
+                logger.LogInformation "Steal cookies successfully."
                 
-        logger.LogInformation "Doing work."
+                let request = RestRequest(Method.POST)
+                cookies |> List.iter (fun cookie -> request.AddCookie(cookie.Name, cookie.Value) |> ignore)
+                
+                request.AddParameter("officeId", " 381") |> ignore
+                request.AddParameter("state", " 2") |> ignore
+                request.AddParameter("filterTourOfferId", " 0") |> ignore
+                
+                match! executeRequestAsync (defaultClient Urls.getListUri, request) with
+                | Ok response ->
+                    logger.LogInformation response.Content
+                | Error error ->
+                    logger.LogError error
+                    
+            | Error error -> logger.LogError error
+            
+        } |> Async.RunSynchronously
 
 let configureLogging (builder : ILoggingBuilder) = 
     builder.AddConsole().AddDebug() |> ignore
