88c1e591a108078e9a30b8fdbe24f07e76e0efb7
Alex <a.katrynets@gmail.com>
2021-04-14 23:21:09 +0300

    simplify code

--- src/Services/Exchange/Exchange.API/Repositories.fs
+++ src/Services/Exchange/Exchange.API/Repositories.fs
@@ -6,35 +6,27 @@ open Monobank
 
 module CurrencyRateRepo =
     
-    type CurrencyRate =
-        { CodeA : int
-          CodeB : int
-          Date  : int
-          Sell  : float
-          Buy   : float
-          Cross : float }
-    
     [<Literal>]
     let private currencyRateKey = "_currencyRate"
     let private options = MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(1.0))
         
-    let update (cache : IMemoryCache) (rates : CurrencyRate []) =
+    let update (cache : IMemoryCache) rates =
         cache.Set(currencyRateKey, rates, options)
     
     let getAsync (cache : IMemoryCache) =
         async {
-            match cache.TryGetValue<CurrencyRate []> currencyRateKey with
+            match cache.TryGetValue currencyRateKey with
             | true, rates -> return rates
             | _ -> 
                 let! jsonData = Currency.AsyncLoad currencyUrl
                 let rates =
                     jsonData
                     |> Array.map (fun curr ->
-                        { CodeA = curr.CurrencyCodeA
-                          CodeB = curr.CurrencyCodeB
-                          Date  = curr.Date
-                          Sell  = curr.RateSell  |> Option.map float |> Option.defaultValue 0.0 
-                          Buy   = curr.RateBuy   |> Option.map float |> Option.defaultValue 0.0
-                          Cross = curr.RateCross |> Option.map float |> Option.defaultValue 0.0 })
-                return update cache rates 
+                        {| CodeA = curr.CurrencyCodeA
+                           CodeB = curr.CurrencyCodeB
+                           Date  = curr.Date
+                           Sell  = curr.RateSell  |> Option.map float |> Option.defaultValue 0.0 
+                           Buy   = curr.RateBuy   |> Option.map float |> Option.defaultValue 0.0
+                           Cross = curr.RateCross |> Option.map float |> Option.defaultValue 0.0 |})
+                return update cache (box rates) 
         }
